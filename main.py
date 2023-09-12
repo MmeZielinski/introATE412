9e79989a59dccac5b2c30fb425bbdcddd5901229
zielinskia <zielinskia@college-montreal.qc.ca>
2023-09-12 14:50:38 -0400

    sdefgrlo;

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print('Bonjour')
+
+#exercice 2: prends deux entiers en paramètre et retourne la somme
+def somme(entier1, entier2):
+    addition = entier1 + entier2
+    #return addition
+    return addition
+
+print(somme(5,8))
