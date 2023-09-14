1e863f19831a224dc3fafa1fd454a2fdad87a591
zielinskia <zielinskia@college-montreal.qc.ca>
2023-09-14 09:32:28 -0400

    sdefgrlo;

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+#exercice 4:
 
-#exercice 2: prends deux entiers en paramètre et retourne la somme
-def somme(entier1, entier2):
-    addition = entier1 + entier2
-    #return addition
-    return addition
+nombre_de_chiffres_pairs = 0
+nombre_de_chiffres_impairs = 0
+for chiffre in range(1,10):
+    if (chiffre%2) == 0:
+        print('%d est pair.'%chiffre)
+        nombre_de_chiffres_pairs +=1
+    else:
+        print('%d est impair.'%chiffre)
+        nombre_de_chiffres_impairs +=1
 
-print(somme(5,8))
+print('Il y a '+str(nombre_de_chiffres_pairs)+' chiffres pairs et '+str(nombre_de_chiffres_impairs)+' chiffres impairs')
